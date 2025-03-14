55c2cd6a8e794f927ede631cada02d5487bab0cb
Gvozdkov Sergey <groza_1337@mail.ru>
2025-03-14 12:36:36 +0300

    Класс для работы с таблицей Users

---
+++ LR3/UsersTable.php
@@ -0,0 +1,41 @@
+<?php
+
+class UsersTable
+{
+    public static function create(
+        string $email, string $password, string $fio, string $birthday, string $address, string $gender, string $interests, string $vk_profile, string $blood_type, string $Rh_factor
+    )
+    {
+        $query = Database::prepare(
+            'INSERT INTO `users` (email, hash_password, FIO, birthday, address, gender, interests, vk_profile, blood_type, Rh_factor) VALUES (:email, :password, :fio, :birthday, :address, :gender, :interests, :vk_profile, :blood_type, :Rh_factor)'
+        );
+        $query->bindValue(':email', $email);
+        $query->bindValue(':password', $password);
+        $query->bindValue(':fio', $fio);
+        $query->bindValue(':birthday', $birthday);
+        $query->bindValue(':address', $address);
+        $query->bindValue(':gender', $gender);
+        $query->bindValue(':interests', $interests);
+        $query->bindValue(':vk_profile', $vk_profile);
+        $query->bindValue(':blood_type', $blood_type);
+        $query->bindValue(':Rh_factor', $Rh_factor);
+
+        if (!$query->execute()) {
+            throw new PDOException('При добавлении пользователя возникла ошибка');
+        }
+    }
+
+    public static function get_by_email(string $email) : array
+    {
+        $query = Database::prepare('SELECT * FROM `users` WHERE `email` = :email LIMIT 1');
+        $query->bindValue(':email', $email);
+        $query->execute();
+
+        $users = $query->fetchAll();
+
+        if (empty($users)) {
+            return [];
+        }
+        return $users[0];
+    }
+}
