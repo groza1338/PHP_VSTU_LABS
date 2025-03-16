0e6aac1bef6adb17fc1416848b204fac52494b8a
Gvozdkov Sergey <groza_1337@mail.ru>
2025-03-16 17:21:12 +0300

    Теперь email подставится и из POST

--- LR3/pages/login.php
+++ LR3/pages/login.php
@@ -19,7 +19,7 @@ require_once($_SERVER['DOCUMENT_ROOT'] . "/LR3/templates/header.php");
                 ?>
                 <div class="mb-3">
                     <label for="exampleInputEmail1" class="form-label">Адрес электронной почты</label>
-                    <input type="email" class="form-control" id="exampleInputEmail1" name="email" value="<?= $_GET['email'] ?? '' ?>" required>
+                    <input type="email" class="form-control" id="exampleInputEmail1" name="email" value="<?= htmlspecialchars($_POST['email'] ?? $_GET['email'] ?? '') ?>" required>
                 </div>
 
                 <div class="mb-3 position-relative">
